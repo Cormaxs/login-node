0e8e2d2e984470ad801d77c49ad2394d1c2b98d7
cormaxs <tomasbarros076@gmail.com>
2025-02-03 12:33:03 -0300

    correccion de correo

--- controllers/correo.js
+++ controllers/correo.js
@@ -4,12 +4,15 @@ export async function enviarCorreoIniciarSesion(user) {
     const id = user._id.toString();
     const config = {
         host: 'mail.proyectosarg.com',
-        port: 465,
-        secure: true, // false para usar TLS
+        port: 587,
+        secure: false, // false para usar TLS
         auth: {
             user: 'soportetecnico@proyectosarg.com',
             pass: 'D#]_h@;E3_eG' // Usa una contraseña de aplicación
-        }
+        },
+        tls: {
+            rejectUnauthorized: false, // Para evitar errores SSL en algunos servidores
+          }
     };
 const mensaje = {
     from: 'soportetecnico@proyectosarg.com',
@@ -18,11 +21,11 @@ const mensaje = {
     html: `
         <h1>Bienvenido ${user.name} ${user.lastName}</h1>
         <p>Para verificar su cuenta haga click en el siguiente enlace:</p>
-        <a href="https://login-node-tbw8.onrender.com/verificar/${encodeURIComponent(id)}" target="_blank">
+        <a href="https://login-node-production-6343.up.railway.app/verificar/${encodeURIComponent(id)}" target="_blank">
             Verificar cuenta
         </a>
         <p>O copiar y pegar el siguiente enlace en tu navegador:</p>
-        <p><b>https://login-node-tbw8.onrender.com/verificar/${encodeURIComponent(id)}</b></p>
+        <p><b>https://login-node-production-6343.up.railway.app/verificar/${encodeURIComponent(id)}</b></p>
     `
 };
 
